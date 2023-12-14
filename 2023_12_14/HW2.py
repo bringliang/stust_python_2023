7ccd562396f6dc9d2edae7c50f14d2041a2b76af
bringliang <112915047+bringliang@users.noreply.github.com>
2023-12-14 12:03:12 +0800

    1214

---
+++ 2023_12_14/HW2.py
@@ -0,0 +1,24 @@
+class health monitoring:
+    def __init__(self, heart_rate, blood_pressure, temperature):
+        self.name=name
+        self.heart_rate=heart_rate
+        self.blood_pressure=blood_pressure
+        self.temperature=temperature
+
+    def get_heart_rate(self):
+        return self.schoolname
+    
+    def get_heart_rate(self,value):
+        self.heart_rate=value
+
+    def get_blood_pressure(self):
+        return self.blood_pressure
+    
+    def get_blood_pressure(self,value):
+        self.blood_pressure=value
+
+    def get_blood_pressure(self):
+        return self.blood_pressure
+    
+    def get_blood_pressure(self,value):
+        self.blood_pressure=value
